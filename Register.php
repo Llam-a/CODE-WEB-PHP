c0fecffdc25249887eca4bb962ebc2d6291e4b65
Seid0nn <115911041+Seid0nn@users.noreply.github.com>
2023-03-07 08:31:34 +0700

    Update Register.php

--- Register.php
+++ Register.php
@@ -1,45 +1,48 @@
 <?php
-session_start();
-// Kiểm tra nếu user đã đăng nhập thì redirect đến trang search
-if (isset($_SESSION['username'])) {
-  header('Location: search.php');
-}
-
-// Kiểm tra form đăng ký được submit
-if (isset($_POST['register'])) {
-  require('config.php');
-  $username = mysqli_real_escape_string($conn, $_POST['username']);
-  $password = mysqli_real_escape_string($conn, $_POST['password']);
-  $email = mysqli_real_escape_string($conn, $_POST['email']);
-
-  // Thêm user vào bảng users
-  $query = "INSERT INTO users (username, password, email) VALUES ('$username', '$password', '$email')";
-  mysqli_query($conn, $query);
-
-  // Lưu thông tin user và redirect đến trang search
-  $_SESSION['username'] = $username;
-  header('Location: search.php');
-}
+   $dbhost = 'localhost';
+   $dbuser = 'root'; // tên người dùng của bạn
+   $dbpass = ''; // mật khẩu của bạn
+   $dbname = 'userdb';
+   $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+   
+   if(! $conn ) {
+      die('Không thể kết nối: ' . mysqli_error());
+   }
+   
+   if(isset($_POST['submit'])){
+      $username = $_POST['username'];
+      $password = $_POST['password'];
+      $id = $_POST['id'];
+      $email = $_POST['email'];
+      
+      $sql = "INSERT INTO users (username,password,id,email) VALUES ('$username', '$password', '$id', '$email')";
+      
+      $result = mysqli_query($conn,$sql);
+      if($result){
+         echo "Tài khoản đã được tạo thành công.";
+      }else{
+         echo "Lỗi: " . mysqli_error($conn);
+      }
+   }
 ?>
 
-<!DOCTYPE html>
 <html>
-<head>
-  <title>Đăng ký</title>
-</head>
-<body>
-  <h1>Đăng ký</h1>
-  <form method="POST" action="">
-    <label>Tên đăng nhập:</label>
-    <input type="text" name="username" required>
-    <br>
-    <label>Mật khẩu:</label>
-    <input type="password" name="password" required>
-    <br>
-    <label>Email:</label>
-    <input type="email" name="email" required>
-    <br>
-    <input type="submit" name="register" value="Đăng ký">
-  </form>
-</body>
+   <body>
+      
+      <h2>Đăng Kí</h2> 
+      
+      <form method="post" action="">
+         Tên đăng nhập:<br>
+         <input type="text" name="username"><br>
+         Mật khẩu:<br>
+         <input type="password" name="password"><br>
+         ID:<br>
+         <input type="text" name="id"><br>
+         Email:<br>
+         <input type="email" name="email"><br>
+         <input type="submit" name="submit" value="Đăng kí">
+      </form>
+      
+   </body>
 </html>
+
