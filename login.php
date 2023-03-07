5c97439dca9200b3f02d2a17ad97d74c9f15d6df
Seid0nn <115911041+Seid0nn@users.noreply.github.com>
2023-03-07 08:32:07 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,45 +1,50 @@
 <?php
-session_start();
-// Kiểm tra nếu user đã đăng nhập thì redirect đến trang search
-if (isset($_SESSION['username'])) {
-  header('Location: search.php');
-}
 
-// Kiểm tra form đăng nhập được submit
-if (isset($_POST['login'])) {
-  require('config.php');
-  $username = mysqli_real_escape_string($conn, $_POST['username']);
-  $password = mysqli_real_escape_string($conn, $_POST['password']);
-
-  // Kiểm tra thông tin đăng nhập có chính xác hay không
-  $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-  $result = mysqli_query($conn, $query);
-
-  if (mysqli_num_rows($result) == 1) {
-    $_SESSION['username'] = $username;
-    header('Location: search.php');
-  } else {
-    $error_msg = 'Tên đăng nhập hoặc mật khẩu không đúng!';
-  }
-}
+   // Kết nối đến database
+   $dbhost = 'localhost';
+   $dbuser = 'root'; // tên người dùng của bạn
+   $dbpass = ''; // mật khẩu của bạn
+   $dbname = 'userdb';
+   $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+   
+   if(! $conn ) {
+      die('Không thể kết nối: ' . mysqli_error());
+   }
+   
+   // Kiểm tra nếu người dùng đã đăng nhập thì chuyển hướng đến Trang chủ
+   session_start();
+   if(isset($_SESSION['username'])){
+      header("location:index.php");
+   }
+   
+   if(isset($_POST['submit'])){
+      $username = $_POST['username'];
+      $password = $_POST['password'];
+      
+      $sql = "SELECT * FROM users WHERE username='$username' and password='$password'";
+      $result = mysqli_query($conn,$sql);
+      
+      if(mysqli_num_rows($result) == 1){
+         $_SESSION['username'] = $username;
+         header("location:index.php");
+      }else{
+         echo "Tên đăng nhập hoặc mật khẩu không chính xác. Vui lòng thử lại.";
+      }
+   }
 ?>
 
-<!DOCTYPE html>
 <html>
-<head>
-  <title>Đăng nhập</title>
-</head>
-<body>
-  <h1>Đăng nhập</h1>
-  <?php if (isset($error_msg)) echo "<p>$error_msg</p>"; ?>
-  <form method="POST" action="">
-    <label>Tên đăng nhập:</label>
-    <input type="text" name="username" required>
-    <br>
-    <label>Mật khẩu:</label>
-    <input type="password" name="password" required>
-    <br>
-    <input type="submit" name="login" value="Đăng nhập">
-  </form>
-</body>
+   <body>
+      
+      <h2>Đăng Nhập</h2> 
+      
+      <form method="post" action="">
+         Tên đăng nhập:<br>
+         <input type="text" name="username"><br>
+         Mật khẩu:<br>
+         <input type="password" name="password"><br>
+         <input type="submit" name="submit" value="Đăng nhập">
+      </form>
+      
+   </body>
 </html>
