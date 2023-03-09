af3906ae5f80bb5199ed94523674f1ffb76c9fc1
Seid0nn <115911041+Seid0nn@users.noreply.github.com>
2023-03-09 12:20:31 +0700

    Update search.php

--- search.php
+++ search.php
@@ -1,6 +1,12 @@
 <?php
 
-   // Kết nối đến database
+// Kiểm tra nếu người dùng đã đăng nhập thì chuyển hướng đến Trang chủ
+   session_start();
+   if(! isset($_SESSION['username'])){
+      header("location:login.php");
+   }  
+ 
+// Kết nối đến database
    $dbhost = 'localhost';
    $dbuser = 'root'; // tên người dùng của bạn
    $dbpass = ''; // mật khẩu của bạn
@@ -10,11 +16,6 @@
    if(! $conn ) {
       die('Không thể kết nối: ' . mysqli_error());
    }
-   
-   // Kiểm tra nếu người dùng đã đăng nhập thì chuyển hướng đến Trang chủ
-   session_start();
-   if(! isset($_SESSION['username'])){
-      header("location:login.php");
      
 // Xử lý tìm kiếm ID
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
